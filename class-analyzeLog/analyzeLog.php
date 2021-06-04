384c7df0312f6024d3c9d0e9d0599f32e324171c
JohnnyJabs <banks01@gmail.com>
2021-06-04 01:00:05 -0400

    .

--- class-analyzeLog/analyzeLog.php
+++ class-analyzeLog/analyzeLog.php
@@ -1,7 +1,7 @@
 <?php
 
-
-class analyzeLog {
+class analyzeLog
+{
 
     public $type;
     public $log;
@@ -11,27 +11,26 @@ class analyzeLog {
      * @param string $type ['apache', 'mysql']
      * @param string $log ['/your/log/location/file.log']
      */
-	function __construct($type, $log ) {
-		$this->log = file_get_contents($log);
+    public function __construct($type, $log)
+    {
+        $this->log = file_get_contents($log);
         $this->type = $type;
-        if($type == 'apache' || $type == 'php') {
-        preg_match_all('/AH\d{5}/',$this->log,$matches);
-        $this->errorCodeCounts = array_count_values($matches);
-		}
-		elseif($type == 'mysql'){
-        //TODO
+        if ($type == 'apache' || $type == 'php') {
+            preg_match_all('/AH\d{5}/', $this->log, $matches);
+            $this->errorCodeCounts = array_count_values($matches);
+        } elseif ($type == 'mysql') {
+            //TODO
         }
-	}
-    
-    function showMostCommon() {
+    }
+
+    public function showMostCommon()
+    {
 
     }
 
-    function showFatal() {
-
+    public function showFatal()
+    {
 
     }
 
-    
 }
-?>
